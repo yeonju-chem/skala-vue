7e948848d5d3dd7a866aacc58f9152fdb00861c6
이연주 <yeonju.chem@gmail.com>
2026-08-10 17:46:47 +0900

    Add basic Vue directive practices (v-html, v-text, v-bind, v-if, v-show, v-for)

--- README.md
+++ README.md
@@ -42,3 +42,20 @@ npm run build
 ```sh
 npm run lint
 ```
+
+## Customization (2026-08-10)
+
+- src/App.vue: HMR 확인용 초기 세팅, 이후 Vue Directive 실습용 컴포넌트들을 순차적으로 import/렌더링
+- src/views/AboutView.vue: h1 텍스트 변경 (HMR 동작 확인)
+- src/components/practices/basic/SampleOne.vue: 반응성(Reactivity) 예제 (일반 변수 vs ref)
+- src/components/practices/basic/SampleTwo.vue: Text Interpolation 예제
+- src/components/practices/basic/VueHtml.vue: v-html 디렉티브 학습
+- src/components/practices/basic/VueHtmlXss.vue: v-html XSS 취약점 실습
+- src/components/practices/basic/VueText.vue: v-text 디렉티브 학습
+- src/components/practices/basic/VueBind.vue: v-bind 기본 (링크/이미지/버튼 비활성화)
+- src/components/practices/basic/VueBindClass.vue: v-bind 클래스 바인딩 (객체/배열 형식)
+- src/components/practices/basic/VueBindStyle.vue: v-bind 스타일 바인딩 (객체/배열 형식)
+- src/components/practices/basic/VueBindShorthand.vue: v-bind same-name shorthand
+- src/components/practices/basic/VueIf.vue: v-if / v-else-if / v-else
+- src/components/practices/basic/VueShow.vue: v-show
+- src/components/practices/basic/VueFor.vue: v-for (배열/객체/배열 내 객체 렌더링)
--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,31 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import SampleOne from './components/practices/basic/SampleOne.vue'
+import SampleTwo from './components/practices/basic/SampleTwo.vue'
+import VueHtml from './components/practices/basic/VueHtml.vue'
+import VueHtmlXss from './components/practices/basic/VueHtmlXss.vue'
+import VueText from './components/practices/basic/VueText.vue'
+import VueBind from './components/practices/basic/VueBind.vue'
+import VueBindClass from './components/practices/basic/VueBindClass.vue'
+import VueBindStyle from './components/practices/basic/VueBindStyle.vue'
+import VueBindShorthand from './components/practices/basic/VueBindShorthand.vue'
+import VueIf from './components/practices/basic/VueIf.vue'
+import VueShow from './components/practices/basic/VueShow.vue'
+import VueFor from './components/practices/basic/VueFor.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div style="padding: 20px">
+    <SampleOne />
+    <SampleTwo />
+    <VueHtml />
+    <VueHtmlXss />
+    <VueText />
+    <VueBind />
+    <VueBindClass />
+    <VueBindStyle />
+    <VueBindShorthand />
+    <VueIf />
+    <VueShow />
+    <VueFor />
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
